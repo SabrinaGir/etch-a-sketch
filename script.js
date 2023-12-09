81900e18bd8aea4f73e6c5548d2448efccb3a5d3
SabrinaGir <116656240+SabrinaGir@users.noreply.github.com>
2023-12-09 08:41:20 -0500

    Add styles.css for div

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ function runGame() {
 
 // function to create a 16*16 grid
 function createGrid() {
-
+    for(let i = 0; i < 16; i++) {
+        console.log('*');
+        for(let j = 0; j < 16; j++) {
+            console.log('*')
+        }
+    }
 }
+
+
+// function to create a div
+function createDiv() {
+    const div = document.createElement('div');
+    
+}   
